a712a2ccb21979aa97ec5054a6734bb0b9e9db6d
Dmitry <farnitin@yandex.ru>
2025-03-01 12:17:06 +0400

    add ShelterAplication

---
+++ src/main/java/ru/shalter/ShelterAplication.java
@@ -0,0 +1,11 @@
+package ru.shalter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class ShelterAplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShelterAplication.class, args);
+    }
+}
